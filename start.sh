9b89e3b45ebb1e754deba84d097416bd13c29d15
mi-ano <ryanmiano68@gmail.com>
2019-08-05 16:08:32 +0300

    changed text

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://ryan:ryan@localhost/pitch'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://ryan:1234@localhost/pitch'
 python manage.py runserver
